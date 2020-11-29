37af3bfa9fca8523fc8eea5b78752cd8e61cfe2a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-11-29 10:03:41 +0300

    Project settings

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.20-RC"
+    application
+}
+
+group = "org.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    main {
+        java.srcDir("src")
+    }
+    test {
+        java.srcDir("test")
+    }
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings.useExperimentalAnnotation("kotlin.time.ExperimentalTime")
+        languageSettings.useExperimentalAnnotation("kotlin.io.path.ExperimentalPathApi")
+        languageSettings.useExperimentalAnnotation("kotlin.ExperimentalStdlibApi")
+    }
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
